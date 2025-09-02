7c3138bba339017597e0e9d0cd4f3e6bc8c4cf79
neilconnor1977 <neilconnor1977@gmail.com>
2025-09-02 08:04:30 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDYWikAuf8M-MlJFK3XREJqaBhIsdpuyuk",
+  authDomain: "football-academy-92125.firebaseapp.com",
+  projectId: "football-academy-92125",
+  storageBucket: "football-academy-92125.firebasestorage.app",
+  messagingSenderId: "563190280709",
+  appId: "1:563190280709:web:89ddedf5886e41e57e626c",
+  measurementId: "G-SWBLH0T81P"
+};
